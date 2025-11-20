c4b8dbe3953a57f21582e979ca68281b7339cb6f
HrodWolfDev <102832449+HrodWolfS@users.noreply.github.com>
2025-11-20 10:18:32 +0100

    mise à jour du schéma utilisateur

--- models/users.js
+++ models/users.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema(
-    {
-        username: String,
-        password: String,
-    }
-)
+const userSchema = mongoose.Schema({
+  firstname: { type: String, required: true },
+  username: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true },
+  token: { type: String },
+});
 
-const User = mongoose.model('user', userSchema);
+const User = mongoose.model("user", userSchema);
 
 module.exports = User;
